e62360f521d1168ef9ea8d7dc94b019731df2056
TypicalDoofus <rawhamer@avans.nl>
2019-03-10 20:32:39 +0100

    Fixed tests to include auth.

--- test/thread_test.js
+++ test/thread_test.js
@@ -4,54 +4,78 @@
 
 /* require necessary modules and files */
 const chai = require('chai');
+const mongoose = require('mongoose');
 const chaiHttp = require('chai-http');
 const server = require('../src/server');
+let jwt = require('jsonwebtoken');
+let bcrypt = require('bcryptjs');
+let config = require('../src/config/config');
+let User = require('../src/model/user');
+let Thread = require('../src/model/thread').thread;
 
 /* chai setup */
 chai.should();
 chai.use(chaiHttp);
 
 describe('Thread API CRUD functionality', () => {
-    it('should return a thread when posting a valid object', (done) => {
-        chai.request(server)
-            .post('/api/user')
-            .send({ username: 'testUser',  password: 'testPassword' })
-            .end((err, res) => {
-                chai.request(server)
-                    .post('/api/thread')
-                    .send({
-                        title: 'testTitle',
-                        content: 'testContent',
-                        username: 'testUser',
-                        upVote: [{
-                            name: 'testUser'
-                        }],
-                        downVote: [{
-                            name: 'testUser'
-                        }]
-                    })
-                    .end((err, res) => {
-                        res.should.have.status(200);
-                        res.should.be.a('object');
 
-                        const body = res.body;
-                        body.should.have.property('title').equals('testTitle');
-                        body.should.have.property('content').equals('testContent');
-                        body.should.have.property('username').equals('testUser');
-                        body.should.have.property('__v').equals(0);
-                        done();
-                    })
+    let testUser, token, testThread;
+
+    /* runs once before each (individual) test */
+    beforeEach(function(done) {
+        this.timeout(10000);
+        testUser = new User({username: 'TestUser', password: bcrypt.hashSync('password')});
+        testThread = new Thread({content: 'content', threadId: 'threadId', username: 'TestUser'});
+        testUser.save(() => {
+            token = jwt.sign({ id: testUser._id }, config.secretkey, {
+                expiresIn: 86400 //Expires in 24 hrs
+            });
+            testThread.save(() =>{
+
+            });
+            done()
+        })
+    });
+
+    it('should return a thread when posting a valid object', (done) => {
+
+        chai.request(server)
+            .post('/api/thread')
+            .set('x-access-token', token)
+            .send({
+                title: 'testTitle',
+                content: 'testContent',
+                username: 'TestUser',
+                upVote: [{
+                    name: 'TestUser'
+                }],
+                downVote: [{
+                    name: 'TestUser'
+                }]
             })
-    }).timeout(5000);
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.should.be.a('object');
+
+                const body = res.body;
+                body.should.have.property('title').equals('testTitle');
+                body.should.have.property('content').equals('testContent');
+                body.should.have.property('username').equals('TestUser');
+                body.should.have.property('__v').equals(0);
+                done();
+            })
+    });
+
 
     it('/GET api should return status 404 when no threads are found', (done) => {
         chai.request(server)
             .get('/api/threads')
+            .set('x-access-token', token)
             .end((err, res) => {
                 res.should.have.status(404);
                 res.should.be.a('object');
 
                 done();
             })
-    }).timeout(5000);
-});
+    })
+}).timeout(5000);
--- test/user_test.js
+++ test/user_test.js
@@ -4,25 +4,46 @@
 
 /* require necessary modules and files */
 const chai = require('chai');
+const mongoose = require('mongoose');
 const chaiHttp = require('chai-http');
 const server = require('../src/server');
+let jwt = require('jsonwebtoken');
+let bcrypt = require('bcryptjs');
+let config = require('../src/config/config');
+let User = require('../src/model/user');
 
 /* chai setup */
 chai.should();
 chai.use(chaiHttp);
 
 describe('User API CRUD functionality', () => {
+
+    let testUser, token;
+
+    /* runs once before each (individual) test */
+    beforeEach(function(done) {
+        this.timeout(10000);
+        testUser = new User({username: 'TestUser', password: bcrypt.hashSync('password')});
+        testUser.save(() => {
+            token = jwt.sign({ id: testUser._id }, config.secretkey, {
+                expiresIn: 86400 //Expires in 24 hrs
+            });
+            done()
+            })
+    });
+
+
+
     it('/POST api should return a user when posting a valid object', (done) => {
         chai.request(server)
-            .post('/api/user')
-            .send({ username: 'testUser',  password: 'testPassword' })
+            .post('/api/register')
+            .send({ username: 'username',  password: 'password' })
             .end((err, res) => {
                 res.should.have.status(200);
                 res.should.be.a('object');
 
                 const body = res.body;
-                body.should.have.property('username').equals('testUser');
-                body.should.have.property('password').equals('testPassword');
+                body.should.have.property('username').equals('username');
                 body.should.have.property('__v').equals(0);
 
                 done();
@@ -31,7 +52,7 @@ describe('User API CRUD functionality', () => {
 
     it('/POST api should return an error code 422 when posting an object without a username', (done) => {
         chai.request(server)
-            .post('/api/user')
+            .post('/api/register')
             .send({ username: '', password: 'testPassword'})
             .end((err, res) => {
                 res.should.have.status(422);
@@ -42,7 +63,7 @@ describe('User API CRUD functionality', () => {
 
     it('/POST api should return an error code 422 when posting an object without a password', (done) => {
         chai.request(server)
-            .post('/api/user')
+            .post('/api/register')
             .send({ username: 'testUser', password: ''})
             .end((err, res) => {
                 res.should.have.status(422);
@@ -53,11 +74,11 @@ describe('User API CRUD functionality', () => {
 
     it('/POST api should return an error code 409 when posting an object with a username that already exists', (done) => {
         chai.request(server)
-            .post('/api/user')
+            .post('/api/register')
             .send({ username: 'testUser', password: 'testPassword'})
             .end(() => {
                 chai.request(server)
-                    .post('/api/user')
+                    .post('/api/register')
                     .send({ username: 'testUser', password: 'testPassword'
                     })
                     .end((err, res) => {
@@ -68,27 +89,11 @@ describe('User API CRUD functionality', () => {
             })
     }).timeout(5000);
 
-    it('/DELETE api should return status 200 when deleting a valid user', (done) => {
-        chai.request(server)
-            .post('/api/user')
-            .send({ username: 'testUser',  password: 'testPassword' })
-            .end((err, res) => {
-                chai.request(server)
-                    .delete('/api/user')
-                    .send({ username: 'testUser', password: 'testPassword'})
-                    .end((err, res) => {
-                        res.should.have.status(200);
-                        res.should.be.a('object');
-
-                        done();
-                    })
-            })
-    }).timeout(5000);
-
     it('/DELETE api should return status 404 when deleting a non-existant user', (done) => {
         chai.request(server)
             .delete('/api/user')
-            .send({ username: 'testUser',  password: 'testPassword' })
+            .set('x-access-token', token)
+            .send({ username: 'testUser', password: 'password'})
             .end((err, res) => {
                 res.should.have.status(404);
                 res.should.be.a('object');
@@ -98,133 +103,118 @@ describe('User API CRUD functionality', () => {
     }).timeout(5000);
 
     it('/DELETE api should return status 401 when the password is incorrect when deleting a valid user', (done) => {
-        chai.request(server)
-            .post('/api/user')
-            .send({ username: 'testUser',  password: 'testPassword' })
-            .end((err, res) => {
-                chai.request(server)
-                    .delete('/api/user')
-                    .send({ username: 'testUser', password: 'testPassword1'})
-                    .end((err, res) => {
-                        res.should.have.status(401);
-                        res.should.be.a('object');
 
-                        done();
-                    })
+        chai.request(server)
+            .delete('/api/user')
+            .set('x-access-token', token)
+            .send({ username: 'TestUser', password: 'testPassword1'})
+            .end((err, res) => {
+                res.should.have.status(401);
+                res.should.be.a('object');
+
+                done();
             })
+
     }).timeout(5000);
 
     it('/DELETE api should return status 500 when the password is omitted when deleting a valid user', (done) => {
-        chai.request(server)
-            .post('/api/user')
-            .send({ username: 'testUser',  password: 'testPassword' })
-            .end((err, res) => {
-                chai.request(server)
-                    .delete('/api/user')
-                    .send({ username: 'testUser', password: ''})
-                    .end((err, res) => {
-                        res.should.have.status(500);
-                        res.should.be.a('object');
 
-                        done();
-                    })
+        chai.request(server)
+            .delete('/api/user')
+            .set('x-access-token', token)
+            .send({ username: 'TestUser', password: ''})
+            .end((err, res) => {
+                res.should.have.status(500);
+                res.should.be.a('object');
+
+                done();
             })
+
     }).timeout(5000);
 
     it('/DELETE api should return status 500 when the username is omitted when deleting a valid user', (done) => {
-        chai.request(server)
-            .post('/api/user')
-            .send({ username: 'testUser',  password: 'testPassword' })
-            .end((err, res) => {
-                chai.request(server)
-                    .delete('/api/user')
-                    .send({ username: '', password: 'testPassword'})
-                    .end((err, res) => {
-                        res.should.have.status(500);
-                        res.should.be.a('object');
 
-                        done();
-                    })
+        chai.request(server)
+            .delete('/api/user')
+            .set('x-access-token', token)
+            .send({ username: '', password: 'password'})
+            .end((err, res) => {
+                res.should.have.status(500);
+                res.should.be.a('object');
+
+                done();
             })
+
     }).timeout(5000);
 
     it('/GET api should return a user when getting a valid object', (done) => {
+
         chai.request(server)
-            .post('/api/user')
-            .send({username: 'testUser', password: 'testPassword'})
+
+            .get('/api/user?username=TestUser')
+            .set('x-access-token', token)
             .end((err, res) => {
-                chai.request(server)
-                    .get('/api/user?username=testUser')
+                res.should.have.status(200);
+                res.should.be.a('object');
 
-                    .end((err, res) => {
-                        res.should.have.status(200);
-                        res.should.be.a('object');
+                const body = res.body;
+                body.should.have.property('username').equals('TestUser');
+                body.should.have.property('__v').equals(0);
 
-                        const body = res.body;
-                        body.should.have.property('username').equals('testUser');
-                        body.should.have.property('password').equals('testPassword');
-                        body.should.have.property('__v').equals(0);
-
-                        done();
-                    })
+                done();
             })
+
     }).timeout(5000);
 
     it('/GET api should return status 404 when a user is not found', (done) => {
+
         chai.request(server)
-            .post('/api/user')
-            .send({ username: 'testUser',  password: 'testPassword' })
+            .get('/api/user?username=testUser1')
+            .set('x-access-token', token)
             .end((err, res) => {
-                chai.request(server)
-                    .get('/api/user?username=testUser1')
+                res.should.have.status(404);
+                res.should.be.a('object');
 
-                    .end((err, res) => {
-                        res.should.have.status(404);
-                        res.should.be.a('object');
-
-                        done()
-                    })
+                done()
             })
+
     }).timeout(5000);
 
     it('/PUT api should return status 200 when putting a valid user', (done) => {
-        chai.request(server)
-            .post('/api/user')
-            .send({ username: 'testUser',  password: 'testPassword' })
-            .end((err, res) => {
-                chai.request(server)
-                    .put('/api/user')
-                    .send({ username: 'testUser', password: 'testPassword', newPassword: 'newTestPassword' })
-                    .end((err, res) => {
-                        res.should.have.status(200);
-                        res.should.be.a('object');
 
-                        done();
-                    })
+        chai.request(server)
+            .put('/api/user')
+            .send({ username: 'TestUser', password: 'password', newPassword: 'newTestPassword' })
+            .set('x-access-token', token)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.should.be.a('object');
+
+                done();
             })
+
     }).timeout(5000);
 
     it('/PUT api should return a status 401 when the password for a user is incorrect', (done) => {
-        chai.request(server)
-            .post('/api/user')
-            .send({ username: 'testUser',  password: 'testPassword' })
-            .end((err, res) => {
-                chai.request(server)
-                    .put('/api/user')
-                    .send({ username: 'testUser',  password: 'newTestPassword', newPassword:'newTestingPassword' })
-                    .end((err, res) => {
-                        res.should.have.status(401);
-                        res.should.be.a('object');
 
-                        done();
-                    })
+        chai.request(server)
+            .put('/api/user')
+            .send({ username: 'TestUser',  password: 'newTestPassword', newPassword:'newTestingPassword' })
+            .set('x-access-token', token)
+            .end((err, res) => {
+                res.should.have.status(401);
+                res.should.be.a('object');
+
+                done();
             })
+
     }).timeout(5000);
 
     it('/PUT should return a status 500 when the user is not found when putting a user', (done) => {
         chai.request(server)
             .put('/api/user')
             .send({ username: 'testUser', password: 'testPassword', newPassword: 'newTestPassword' })
+            .set('x-access-token', token)
             .end((err, res) => {
                 res.should.have.status(404);
                 res.should.be.a('object');
@@ -234,53 +224,60 @@ describe('User API CRUD functionality', () => {
     }).timeout(5000);
 
     it('/PUT should return a status 500 when the username for putting a user is omitted', (done) => {
-        chai.request(server)
-            .post('/api/user')
-            .send({ username: 'testUser',  password: 'testPassword' })
-            .end((err, res) => {
-                chai.request(server)
-                    .put('/api/user')
-                    .send({ username: '',  password: 'newTestPassword', newPassword:'newTestingPassword' })
-                    .end((err, res) => {
-                        res.should.have.status(500);
-                        res.should.be.a('object');
 
-                        done();
-                    })
+        chai.request(server)
+            .put('/api/user')
+            .send({ username: '',  password: 'newTestPassword', newPassword:'newTestingPassword' })
+            .set('x-access-token', token)
+            .end((err, res) => {
+                res.should.have.status(500);
+                res.should.be.a('object');
+
+                done();
             })
+
     }).timeout(5000);
 
     it('/PUT should return a status 500 when the password for putting a user is omitted', (done) => {
-        chai.request(server)
-            .post('/api/user')
-            .send({ username: 'testUser',  password: 'testPassword' })
-            .end((err, res) => {
-                chai.request(server)
-                    .put('/api/user')
-                    .send({ username: 'testUser',  password: '', newPassword:'newTestingPassword' })
-                    .end((err, res) => {
-                        res.should.have.status(500);
-                        res.should.be.a('object');
 
-                        done();
-                    })
+        chai.request(server)
+            .put('/api/user')
+            .send({ username: 'TestUser',  password: '', newPassword:'newTestingPassword' })
+            .set('x-access-token', token)
+            .end((err, res) => {
+                res.should.have.status(500);
+                res.should.be.a('object');
+
+                done();
             })
+
     }).timeout(5000);
 
     it('/PUT should return a status 500 when the new password for putting a user is omitted', (done) => {
-        chai.request(server)
-            .post('/api/user')
-            .send({ username: 'testUser',  password: 'testPassword' })
-            .end((err, res) => {
-                chai.request(server)
-                    .put('/api/user')
-                    .send({ username: 'testUser',  password: '', newPassword:'' })
-                    .end((err, res) => {
-                        res.should.have.status(500);
-                        res.should.be.a('object');
 
-                        done();
-                    })
+        chai.request(server)
+            .put('/api/user')
+            .send({ username: 'TestUser',  password: 'password', newPassword:'' })
+            .set('x-access-token', token)
+            .end((err, res) => {
+                res.should.have.status(500);
+                res.should.be.a('object');
+
+                done();
+            })
+
+    }).timeout(5000);
+
+    it('/DELETE api should return status 200 when deleting a valid user', (done) => {
+        chai.request(server)
+            .delete('/api/user')
+            .set('x-access-token', token)
+            .send({ username: 'TestUser', password: 'password'})
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.should.be.a('object');
+
+                done();
             })
     }).timeout(5000);
 });
